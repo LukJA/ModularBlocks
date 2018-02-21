5baf86f12f4d571231f0e9b53b44dd79b4230177
Luke Andrews <lukejandrews@hotmail.co.uk>
2018-02-21 20:44:22 +0000

    cleaned up package and added some useful functions

--- pack_global.vhd
+++ pack_global.vhd
@@ -4,6 +4,8 @@
 /* (c) Luke Andrews 2017 */
 /* All software is written under the MIT license and available on Github */
 
+/* this package contains the types and functions needed to implement the K1 standard */
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -17,39 +19,66 @@ package global is
    type vector_t is std_logic_vector;
   
    -- functions to convert between boolean / std_[u]logic.
-	function to_std (constant val : in boolean) return std_logic;
-	function to_bool (constant val : in std_logic) return boolean;
-	function to_unsig (x: in std_logic) return unsigned;
+	function bool_to_std (constant val : in boolean) return std_logic;
+	function std_to_bool (constant val : in std_logic) return boolean;
+	function std_to_unsig (x: in std_logic) return unsigned;
+	function std_to_vec (x: in std_logic) return std_logic_vector;
+	function vec_to_std (x: in std_logic_vector) return std_logic;
+	
+	
   
 end package;
 
 package body global is
 
-	function to_std (constant val : in boolean) return std_logic is
+	/* function to convert boolean value to std_logic */
+	function bool_to_std (constant val : in boolean) return std_logic is
 	begin
 		if val then
 			return '1';
 		else
 			return '0';
 		end if; 
-	end function to_std;
+	end function bool_to_std;
 	
-	function to_bool (constant val : in std_logic) return boolean is
+	/* function to convert std_logic value to bool */
+	function std_to_bool (constant val : in std_logic) return boolean is
 	begin
 		if val then
 			return true;
 		else
 			return false;
 		end if; 
-	end function to_bool;
+	end function std_to_bool;
 	
-	function to_unsig (x: in std_logic) return unsigned is
+	/* function to convert std_logic value to unsigned */
+	function std_to_unsig (x: in std_logic) return unsigned is
 	begin
 		if x='1' then
 			return "1";
 		else 
 			return "0"; 
 		end if;
-	end function to_unsig;
+	end function std_to_unsig;
 
+	
+	/* function to convert std_logic value to std_logic_vector */
+	function std_to_vec (x: in std_logic) return std_logic_vector is
+	begin
+		if x='1' then
+			return "1";
+		else 
+			return "0"; 
+		end if;
+	end function std_to_vec;
+	
+	/* function to convert std_logic_vector value to std_logic */
+	function vec_to_std (x: in std_logic_vector) return std_logic is
+	begin
+		if x="1" then
+			return '1';
+		else 
+			return '0'; 
+		end if;
+	end function vec_to_std;
 end package body global;
